6e663cb9ede8d16f156593beb32798835318ce81
Ángel <49351992+chelito504@users.noreply.github.com>
2019-04-16 23:04:31 -0600

    Update conexiones.py

--- conexiones.py
+++ conexiones.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 import sys
 import os.path as path
@@ -13,33 +12,51 @@ class Principal(QtGui.QDialog):
 		self.uiPrincipal.setupUi(self)
 		QtCore.QObject.connect(self.uiPrincipal.toolButtonOrigen, QtCore.SIGNAL('clicked()'), self.abrirBusqueda)
 		QtCore.QObject.connect(self.uiPrincipal.toolButtonDestino, QtCore.SIGNAL('clicked()'), self.abrirBusqueda)
-		QtCore.QObject.connect(self.uiPrincipal.pushButtonEncriptarAES, QtCore.SIGNAL('clicked()'), self.abrirEncriptarAes)
+		QtCore.QObject.connect(self.uiPrincipal.pushButtonEncriptarAES, QtCore.SIGNAL('clicked()'), self.abrirEncriptamiento)
 		QtCore.QObject.connect(self.uiPrincipal.pushButton, QtCore.SIGNAL('clicked()'), self.salirPrincipal)
-	def comprobarExistenciaOrigen(self):
-		if path.exists(self.uiPrincipal.lineEditOrigen.text()):
-			return True
+	def abrirEncriptamiento(self):
+		#if len(self.uiPrincipal.lineEditOrigen.text())==0:
+		#	return 0
+		#else:
+		self.objectEncriptamiento1 = EncriptamientoAES()
+		if (path.exists(self.uiPrincipal.lineEditOrigen.text()) and path.exists(self.uiPrincipal.lineEditDestino.text())):
+			self.objectEncriptamiento1.mostrarEncriptamientoAES()
+			self.uiPrincipal.lineEditOrigen.clear()
+			self.uiPrincipal.lineEditDestino.clear()
 		else:
-			return False
-	def comprobarExistenciaDestino(self):
-		if path.exists(self.uiPrincipal.lineEditDestino.text()):
-			return True
-		else:
-			return False
+			self.uiPrincipal.label_3.setText('ERROR: comprobar ruta/s')
+	#def comprobarExistenciaDestino(self):
+		#if len(self.uiPrincipal.lineEditDestino.text())==0:
+		#	return 0
+		#else:
+		#if path.exists(self.uiPrincipal.lineEditDestino.text()):
+		#	return True
+		#else:
+		#	return False
 	def abrirBusqueda(self):
 		self.objectBusqueda = Busqueda()
 		self.objectBusqueda.mostrarBusqueda()
 	def salirPrincipal(self):
 		sys.exit()
-	def abrirEncriptarAes(self):
-		self.objectEncriptamiento1 = EncriptamientoAES()
-		if (self.comprobarExistenciaOrigen==True and self.comprobarExistenciaDestino==False):
-			self.uiPrincipal.label_3.setText('ERROR: verificar destino')
-		elif (self.comprobarExistenciaOrigen==False and self.comprobarExistenciaDestino==True):
-			self.uiPrincipal.label_3.setText('ERROR: verificar origen')
-		elif (self.comprobarExistenciaOrigen==False and self.comprobarExistenciaDestino==False):
-			self.uiPrincipal.label_3.setText('ERROR: verificar ambas rutas')
-		else:
-			self.objectEncriptamiento1.mostrarEncriptamientoAES()
+	#def abrirEncriptarAes(self):
+	#	if(self.comprobarExistenciaOrigen==True and self.comprobarExistenciaDestino==True):
+	#		self.objectEncriptamiento1 = EncriptamientoAES()
+	#		self.objectEncriptamiento1.mostrarEncriptamientoAES()
+	#		self.uiPrincipal.lineEditOrigen.clear()
+	#		self.uiPrincipal.lineEditDestino.clear()
+		#elif(self.comprobarExistenciaOrigen==True and self.comprobarExistenciaDestino==False):
+		#	self.uiPrincipal.label_3.setText('ERROR: verificar destino')
+		#elif(self.comprobarExistenciaOrigen==False and self.comprobarExistenciaDestino==True):
+		#	self.uiPrincipal.label_3.setText('ERROR: verificar origen')
+	#	else:
+		#if (self.comprobarExistenciaOrigen==False and self.comprobarExistenciaDestino==False):
+	#		self.uiPrincipal.label_3.setText('ERROR: verificar ambas rutas')
+		#elif (self.comprobarExistenciaOrigen==0 and self.comprobarExistenciaDestino==0):
+		#	self.uiPrincipal.label_3.setText('ERROR: ambas rutas vacias')
+		#else:
+			#self.objectEncriptamiento1.mostrarEncriptamientoAES()
+			#self.uiPrincipal.lineEditOrigen.clear()
+			#self.uiPrincipal.lineEditDestino.clear()
 class Busqueda:
 	def mostrarBusqueda(self):
 		self.objectBusquedaOpen = Busqueda2()
