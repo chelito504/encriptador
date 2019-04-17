09b421ac6f7d78baf1a566e7fd6451956e2aeb3f
Ángel <49351992+chelito504@users.noreply.github.com>
2019-04-16 18:51:59 -0600

    Update Contrasena.py

--- Contrasena.py
+++ Contrasena.py
@@ -15,7 +15,7 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
-class Ui_Dialog(object):
+class Ui_DialogContrasena(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName(_fromUtf8("Dialog"))
         Dialog.resize(318, 142)
